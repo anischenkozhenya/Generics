9864e467bf98944b8f55182a7bdf1aa33ae9dbd4
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-09-15 15:16:03 +0300

    +task3

---
+++ Generics/Task3/MyDictionary.cs
@@ -0,0 +1,73 @@
+﻿namespace Task3
+{
+    class MyDictionary<Tkey, TValue>
+    {
+        private Tkey[] key;
+        private TValue[] value;
+        private int length;
+        public MyDictionary()
+        {
+            key = new Tkey[0];
+            value = new TValue[0];
+            length = 0;
+        }
+        public void Add(Tkey tkey, TValue tvalue)
+        {
+
+            var key1 = new Tkey[length + 1];
+            var value1 = new TValue[length + 1];
+            for (int i = 0; i < length; i++)
+            {
+                key1[i] = key[i];
+                value1[i] = value[i];
+            }
+            length++;
+            key1[key1.Length - 1] = tkey;
+            value1[value1.Length - 1] = tvalue;
+            key = key1;
+            value = value1;
+        }
+        public override string ToString()
+        {
+            string str = null;
+            if (length > 0)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    str += key[i] + " " + value[i] + "\n";
+                }
+
+            }
+            else
+                str += "Null\n";
+            return str;
+        }
+        public void Clear()
+        {
+            key = new Tkey[0];
+            value = new TValue[0];
+            length = 0;
+        }
+        public int Count
+        {
+            get
+            {
+                return length;
+            }
+        }
+        public string this[int index]
+        {
+            get
+            {
+                if (index > 0 && index < length)
+                {
+                    return value[index].ToString() + "-" + key[index].ToString();
+                }
+                else
+                {
+                    return "в словане нет такого";
+                }
+            }
+        }
+    }
+}
---
+++ Generics/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Создайте класс MyDictionary<TKey, TValue>.Реализуйте в простейшем приближении возможность
+//использования его экземпляра аналогично экземпляру класса Dictionary(Урок 6 пример 5).
+//Минимально требуемый интерфейс взаимодействия с экземпляром, должен включать метод
+//добавления пар элементов, индексатор для получения значения элемента по указанному индексу и
+//свойство только для чтения для получения общего количества пар элементов.
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var my = new MyDictionary<int, string>();
+            my.Add(100, "qwerty");
+            my.Add(200, "qwerty1");
+            my.Add(300, "qwerty2");
+            Console.WriteLine(my.Count);
+            Console.WriteLine(my.ToString());
+            Console.WriteLine(my[1]);
+            Console.WriteLine(my[2]);
+            my.Clear();
+            Console.WriteLine(my.Count);
+            Console.WriteLine(my.ToString());
+            Console.ReadKey();
+        }
+    }
+}
